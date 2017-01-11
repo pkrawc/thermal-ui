7b8f616d3d89a19f43829cf9a738a8a92697370d
Patrick Krawczykowski <pkrawz@gmail.com>
2017-01-10 22:49:10 -0600

    upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,76 @@
-const path = require('path')
+const { join, resolve } = require('path')
 const webpack = require('webpack')
-const WebpackHtmlWebpackPlugin = require('html-webpack-plugin')
 const prod = process.env.NODE_ENV === 'production'
 
 const config = {
   cache: true,
-  debug: true,
   devtool: prod ? 'eval' : 'inline-source-map',
   devServer: {
     hot: true,
-    quite: true
+    inline: true,
+    contentBase: resolve(__dirname, 'docs'),
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    quiet: false,
+    noInfo: false,
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: true,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: true,
+      errorDetails: true,
+      warnings: false,
+      publicPath: true
+    }
   },
   entry: {
-    'docs/' : './src/example.js',
-    'build/' : './src/index.js'
+    'example' : [resolve(__dirname, 'src/example.js')],
+    'index' : resolve(__dirname, 'src/index.js')
   },
   output: {
-    path: path.join(__dirname, '/'),
-    publicPath: '/',
-    filename: '[name]index.js',
+    path: prod ? join(__dirname, 'build') : join(__dirname, 'docs'),
+    filename: '[name]-build.js',
     chunkFilename: '[id].js',
     library: 'thermal-ui',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
   module: {
-    loaders: [
-      { test: /\.(jsx?|es6)$/, loader: 'babel', exclude: [/node_modules/] }
+    rules: [
+      { test: /\.(jsx?|es6)$/, loader: 'babel-loader', exclude: [/node_modules/] }
     ]
   },
   plugins: prod ? [
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
-      mangle: false,
-      compress: {
-        warnings: false
-      }
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
     }),
-    new WebpackHtmlWebpackPlugin({
-      template: 'src/example.html',
-      filename: 'docs/index.html',
-      excludeChunks: ['build/']
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ] : [
-    new webpack.NoErrorsPlugin(),
-    new WebpackHtmlWebpackPlugin({
-      template: 'src/example.html'
-    })
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+       debug: true
+     })
   ],
   resolve: {
-    root: path.join(__dirname),
-    exclude: /node_modules/,
-    extensions: ['', '.js', '.jsx', '.es6']
+    modules: [
+      join(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx', '.es6']
   }
 }
 
