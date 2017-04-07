0674d035a19f22301f12b9691111b8ee8db16ea7
Patrick Krawczykowski <pkrawz@gmail.com>
2017-04-07 14:48:58 -0500

    hotfix for tests

--- Fields/Input/Input.test.js
+++ Fields/Input/Input.test.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { Input } from '../Input'
+import TextInput from '../Input'
 
 describe('Input', () => {
   const props = {
-    id: '1',
     label: 'input label',
-    type: 'text'
+    type: 'text',
+    test: {
+      id: '1234'
+    }
   }
-  const tree = renderer.create(<Input {...props} />).toJSON()
-
+  const tree = renderer.create(<TextInput {...props} />).toJSON()
+  // need to set the id's for input and label since they're generated on the fly by shorid
+  tree.children[0].props.id = '123'
+  tree.children[1].props.htmlFor = '123'
   it('renders without crashing', () => {
     expect(tree).toMatchSnapshot()
   })
--- General/Button/button.test.js
+++ General/Button/button.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { Button } from '../Button'
+import Button from '../Button'
 
 describe('Button', () => {
 
