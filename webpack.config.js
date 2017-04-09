10850f23af917f0aa116d5415135aa5b8111d799
Patrick Krawczykowski <pkrawz@gmail.com>
2017-04-09 10:21:24 -0500

    whoops, return an object

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,38 @@ const webpack = require('webpack')
 const env = process.env.NODE_ENV
 
 module.exports = function(env) {
-  cache: true,
-  entry: resolve(__dirname, 'index.js'),
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
-    'react-router-dom': 'react-router-dom',
-    'styled-components': 'styled-components'
-  },
-  output: {
-    path: resolve(__dirname, 'build'),
-    filename: 'index.build.js',
-    library: 'thermal-ui',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [
-      {test: /\.(jsx?|es6)$/, loader: 'babel-loader', exclude: [/node_modules/]}
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ],
-  resolve: {
-    modules: [
-      resolve(__dirname, 'src'),
-      'node_modules'
+  return {
+    cache: true,
+    entry: resolve(__dirname, 'index.js'),
+    externals: {
+      react: 'react',
+      'react-dom': 'react-dom',
+      'react-router-dom': 'react-router-dom',
+      'styled-components': 'styled-components'
+    },
+    output: {
+      path: resolve(__dirname, 'build'),
+      filename: 'index.build.js',
+      library: 'thermal-ui',
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    module: {
+      rules: [
+        {test: /\.(jsx?|es6)$/, loader: 'babel-loader', exclude: [/node_modules/]}
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      })
     ],
-    extensions: ['.js', '.jsx', '.es6']
+    resolve: {
+      modules: [
+        resolve(__dirname, 'src'),
+        'node_modules'
+      ],
+      extensions: ['.js', '.jsx', '.es6']
+    }
   }
 }
